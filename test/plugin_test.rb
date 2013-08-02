de7dc2f114d76374bbb9531873773e025192a42e
Andri Möll <andri@dot.ee>
2013-08-03 02:24:30 +0300

    Don't change Vim's directory.

--- test/plugin_test.rb
+++ test/plugin_test.rb
@@ -2,25 +2,19 @@ require_relative "./helper"
 
 describe "Plugin" do
   describe "b:node_root" do
-    must_set_node_root = lambda do |dir|
-      $vim.edit "index.js"
-      # Mac has the temporary directory symlinked, so need File.realpath.
-      $vim.echo("b:node_root").must_equal File.realpath(dir)
-    end
-
     it "must be set when in same directory with package.json" do
       Dir.mktmpdir do |dir|
         FileUtils.touch File.join(dir, "package.json")
-        $vim.command("cd #{dir}")
-        must_set_node_root.call dir
+        $vim.edit File.join(dir, "index.js")
+        $vim.echo("b:node_root").must_equal dir
       end
     end
 
     it "must be set when in same directory with node_modules" do
       Dir.mktmpdir do |dir|
         Dir.mkdir File.join(dir, "node_modules")
-        $vim.command("cd #{dir}")
-        must_set_node_root.call dir
+        $vim.edit File.join(dir, "index.js")
+        $vim.echo("b:node_root").must_equal dir
       end
     end
 
@@ -30,8 +24,8 @@ describe "Plugin" do
 
         nested = File.join(dir, "lib", "awesomeness")
         FileUtils.mkdir_p nested
-        $vim.command("cd #{nested}")
-        must_set_node_root.call dir
+        $vim.edit File.join(nested, "index.js")
+        $vim.echo("b:node_root").must_equal dir
       end
     end
 
@@ -41,18 +35,17 @@ describe "Plugin" do
 
         nested = File.join(dir, "lib", "awesomeness")
         FileUtils.mkdir_p nested
-        $vim.command("cd #{nested}")
-        must_set_node_root.call dir
+        $vim.edit File.join(nested, "index.js")
+        $vim.echo("b:node_root").must_equal dir
       end
     end
 
-    it "must detect Node root also for other filetypes" do
+    it "must be set also for other filetypes" do
       Dir.mktmpdir do |dir|
         FileUtils.touch File.join(dir, "package.json")
 
-        $vim.command("cd #{dir}")
-        $vim.edit "README.txt"
-        $vim.echo("b:node_root").must_equal File.realpath(dir)
+        $vim.edit File.join(dir, "README.txt")
+        $vim.echo("b:node_root").must_equal dir
       end
     end
   end
