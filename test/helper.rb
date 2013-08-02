d344b72a926d464feb7a116a596644ad3b081280
Andri Möll <andri@dot.ee>
2013-08-03 02:01:25 +0300

    Remove double VIMRUNNER_RC sourcing as Vimrunner does it itself.

--- test/helper.rb
+++ test/helper.rb
@@ -4,6 +4,4 @@ require "tempfile"
 
 vimrc = File.expand_path("../vimrc", __FILE__)
 $vim = Vimrunner::Server.new(:vimrc => vimrc).start
-$vim.source Vimrunner::Server::VIMRUNNER_RC
-
 Minitest.after_run { $vim.kill }
